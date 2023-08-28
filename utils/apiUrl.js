56cf8b59e74fc9d4f92a9a8791f99e3880dcb4d0
RAJATsd <rajatsharmaatri@gmail.com>
2023-08-28 19:31:10 +0530

    chore: url adjustment

--- utils/apiUrl.js
+++ utils/apiUrl.js
@@ -1,3 +1,9 @@
-const apiUrl = (url) => `http://localhost:3001/${url}`;
+const baseUrl = () =>
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1"
+    ? "http://localhost:3001"
+    : "https://movie-db-backend-vujl.onrender.com";
+
+const apiUrl = (url) => `${baseUrl()}/${url}`;
 
 export default apiUrl;
